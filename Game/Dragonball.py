f81cd58902558f965dae6a6eca22166c59591a3d
기근화 사원 지능화컨설팅팀 (wc1220@lgcns.com <80504@CORP.LGCNS.COM>
2018-01-09 23:54:57 +0900

    dragonball 임시 완성

--- Game/Dragonball.py
+++ Game/Dragonball.py
@@ -4,7 +4,7 @@ import matplotlib.image as mpimg
 import matplotlib.pyplot as plt;
 class User1:
     attacks = [1,2,3];
-    properties = {"name":"KKH","status":0,"energy":0}
+    properties = {"name":"근화","status":0,"energy":0}
     old_properties = {"name":"KKH","status":0,"energy":0}
     # -1 :막기
     # 0 : 으
@@ -12,7 +12,7 @@ class User1:
     # 2 : 에너지파 3>>1 그러나,막기로 막을 수 있음
     # 3 : 원기옥 
     # 기 상태
-    strategy = [0,0,0];
+    strategy = [0,0,0,3,0,-1,1];
     def __init__(self):
         pass
     def next_step(self,step):
@@ -43,16 +43,31 @@ class User1:
         elif status ==1:
             img = mpimg.imread("user1_1.jpg")
         elif status ==-1:
-            img = mpimg.imread("user1_0.jpg")
+            img = mpimg.imread("user1_-1.jpg")
         elif status ==2:
             img = mpimg.imread("user1_2.jpg")
         else:
             img = mpimg.imread("user1_3.jpg")
         plt.imshow(img);
-        plt.show(); 
+        plt.show();
+    def show_status(self):
+        status =  self.properties['status']
+        if status == 0:
+            return "으~"
+        elif status == -1:
+            return "막기"
+        elif status == 1:
+            return "파!"
+        elif status == 2:
+            return "에너지파!"
+        else:
+            return "원기옥"
+        
+    def show_energy(self):
+        return str(self.properties['energy'])
 class User2:
     attacks = [1,2,3];
-    properties = {"name":"SHS","status":0,"energy":0}
+    properties = {"name":"현석","status":0,"energy":0}
     old_properties = {"name":"SHS","status":0,"energy":0}
     # -1 :막기
     # 0 : 으
@@ -60,7 +75,7 @@ class User2:
     # 2 : 에너지파 3>>1 그러나,막기로 막을 수 있음
     # 3 : 원기옥 
     # 기 상태
-    strategy = [0,0,1];
+    strategy = [0,0,0,3,0,1,0];
     def __init__(self):
         pass
     def next_step(self,step):
@@ -91,13 +106,27 @@ class User2:
         elif status ==1:
             img = mpimg.imread("user2_1.jpg")
         elif status ==-1:
-            img = mpimg.imread("user2_0.jpg")
+            img = mpimg.imread("user2_-1.jpg")
         elif status ==2:
             img = mpimg.imread("user2_2.jpg")
         else:
             img = mpimg.imread("user2_3.jpg")
         plt.imshow(img);
         plt.show();
+    def show_status(self):
+        status =  self.properties['status']
+        if status == 0:
+            return "으~"
+        elif status == -1:
+            return "막기"
+        elif status == 1:
+            return "파!"
+        elif status == 2:
+            return "에너지파!"
+        else:
+            return "원기옥"
+    def show_energy(self):
+        return str(self.properties['energy'])
         
 if __name__ == "__main__":
     user1 = User1();
@@ -111,9 +140,11 @@ if __name__ == "__main__":
     i = 0;
     plt.ion();
     while(flag):
+        print("Turn"+str((i+1)))
         user1.next_step(i);
         user2.next_step(i);
-        print(user1.properties['name']+' : '+str(user1.properties['status'])+' VS '+user2.properties['name']+' : '+str(user2.properties['status']));
+        print(user1.properties['name']+' : '+user1.show_status()+' VS '+user2.properties['name']+' : '+user2.show_status());
+        print("Energy : "+user1.properties['name']+' = '+user1.show_energy()+' VS '+user2.properties['name']+' = '+user2.show_energy());
         if user1.properties['status'] !=user2.properties['status']:
             if user1.properties['status'] == 3:
                 print(user1.properties['name']+" Win!");
@@ -139,4 +170,4 @@ if __name__ == "__main__":
         user1.afterTurn();
         user2.afterTurn();
         i+=1;
-        _ = input("Turn"+str((i))+": Press [enter] to continue.")
+        _ = input("Press [enter] to continue.")
